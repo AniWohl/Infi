b71b370f8e4c5255ba542f74b18612ab5c2a2178
Anastasia Wohlmuth <anastasia.wohlmuth@student.htldornbirn.at>
2023-10-19 15:23:20 +0200

    Informatik

---
+++ src/at/woa/Würfelspiel.java
@@ -0,0 +1,4 @@
+package at.woa;
+
+public class Würfelspiel {
+}
--- src/at/woa/e.java
+++ src/at/woa/e.java
@@ -4,13 +4,20 @@ import java.util.Random;
 
 public class e {
     public static void main(String[] args) {
-    }
-    int summe=0;
-
-     {
+        int summe = 0;
+        boolean isFinished = true;
         Random random = new Random();
-        int i = random.nextInt(30) + 10;
-         System.out.println(i);
-         summe+=i
-}    while ( i!=15 // i!=25)
+
+        while (isFinished) {
+            int i = random.nextInt(21) + 10;
+            System.out.println("Generierte Zufallszahl: " + i);
+            summe += i;
+
+            if (i == 15 || i == 25) {
+                isFinished = false;
+            }
+        }
+
+        System.out.println("Die Summe der vorherigen Zufallszahlen ist: " + summe);
+    }
 }
