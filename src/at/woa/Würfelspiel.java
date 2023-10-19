cdf2eb858ac512ac9858515abaed14dfc7f796a9
Anastasia Wohlmuth <anastasia.wohlmuth@student.htldornbirn.at>
2023-10-19 16:12:50 +0200

    infi

--- src/at/woa/Würfelspiel.java
+++ src/at/woa/Würfelspiel.java
@@ -1,4 +1,36 @@
 package at.woa;
 
+import java.util.Random;
+
 public class Würfelspiel {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int winsOfPlayer=0;
+        int winsOfComputer=0;
+        int amountOfDraws=0;
+
+        for(int i =0; i<6; i++) {
+            int randomPlayer = random.nextInt(6) + 1;
+            int RandomComputer = random.nextInt(6) + 1;
+
+            if (randomPlayer>RandomComputer) {
+               winsOfPlayer++;
+            } else if (randomPlayer<RandomComputer) {
+                winsOfComputer++;
+            }
+            else  {
+                amountOfDraws++;
+    }
+            System.out.println("Computer " + winsOfComputer + " Player " + winsOfPlayer + " Draw " + amountOfDraws);
+
+
+        }
+
+
+
+
+
+    }
 }
+
+
