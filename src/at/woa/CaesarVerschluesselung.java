51d496c9bd41c2d4b25d8fc7304962cbac973e03
Anastasia Wohlmuth <anastasia.wohlmuth@student.htldornbirn.at>
2023-11-16 15:30:01 +0100

    infi

--- src/at/woa/CaesarVerschluesselung.java
+++ src/at/woa/CaesarVerschluesselung.java
@@ -2,7 +2,7 @@ package at.woa;
 
 public class CaesarVerschluesselung {
     public static void main(String[] args) {
-        String result = encrypt( shift: 1, stringToencrypt: "hallo" )
+        String result = encrypt( shift,  "hallo" );
     }
 
     public static String encrypt(int shift, String stringToencrypt){
