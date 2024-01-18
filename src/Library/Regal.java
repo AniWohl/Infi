db1b3bb60c5c6518d362d21434aab9d4847f781f
Anastasia Wohlmuth <anastasia.wohlmuth@student.htldornbirn.at>
2024-01-18 16:24:23 +0100

    Katze

--- src/Library/Regal.java
+++ src/Library/Regal.java
@@ -1,4 +1,77 @@
 package Library;
 
+import java.util.ArrayList;
+
 public class Regal {
+    private int Platz;
+    private String Ort;
+    private int anzahlRegale;
+    private int maxBuch;
+
+    private int maxkatzenalter;
+    private ArrayList<Katze> katzenliste;
+
+    public int getMaxkatzenimhaus() {
+        return maxkatzenimhaus;
+    }
+    public int getMaxkatzenalter() {
+        return maxkatzenalter;
+    }
+
+    public void setMaxkatzenimhaus(int maxkatzenimhaus) {
+        this.maxkatzenimhaus = maxkatzenimhaus;
+
+    }
+    public void setmaxkatzenalter(int maxkatzenalter) {
+        this.maxkatzenalter = maxkatzenalter;
+    }
+
+    public Haus(int flaeche, String adresse, int anzahlZimmer, int maxkatzenimhaus) {
+        this.flaeche = flaeche;
+        this.adresse = adresse;
+        this.anzahlZimmer = anzahlZimmer;
+        this.katzenliste = new ArrayList<>();
+        this.maxkatzenimhaus = maxkatzenimhaus;
+    }
+
+    public int getFlaeche() {
+        return flaeche;
+    }
+
+    public void setFlaeche(int flaeche) {
+        this.flaeche = flaeche;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public int getAnzahlZimmer() {
+        return anzahlZimmer;
+    }
+
+    public void setAnzahlZimmer(int anzahlZimmer) {
+        this.anzahlZimmer = anzahlZimmer;
+    }
+
+    public void getInfo () {
+        System.out.println("Adresse " + this.adresse + "\nFlaeche:" + this.flaeche + "\nAnzahl der Zimmer:" + this.anzahlZimmer);
+    }
+
+    public void addKatze(Katze katze) { //
+        if (katzenliste.size() >= this.maxkatzenimhaus) {
+            System.out.println("Der/Die ChefIn will nicht mehr Katzen als " + this.maxkatzenimhaus + " im Haus!!");
+        } else {
+            katzenliste.add(katze);
+        }
+
+    } public void getKatzenImHaus() {
+        for (Katze k : katzenliste) {
+            k.getInfo();
+        }
+    }
 }
--- src/Library/Testit.java
+++ src/Library/Testit.java
@@ -5,13 +5,13 @@ import java.util.Random;
 public class Testit {
     public static void main(String[]args){
         Random r =new Random();
-        ArrayList<Buch> mKBuchListe= new ArrayList();
+        ArrayList<Buch> mBuchListe= new ArrayList();
         Buch buch = new Buch ();
-        String[] GenreListe= {"bösartige","gutartige","listige","liebliche"};
-        String[] AutorListe= {"nackte","flauschige","Kurzhaarige","tode"};
-        int[] SeitenanzahlListe= {3,2,1,4,9,5,6,7};
-        String[] TitelListe= {"pink","blau","weiß","schwarz","grau","orange","braun","fleckig"};
-        String[] JahrListe= {"Sphinx","Snowflake","Luna","Honey","Dewy","Dewdrop","Cake","Cinnamon"};
+        String[] GenreListe= {"Danmei","Fantasy","Horror","Detektive"};
+        String[] AutorListe= {"Rou Bao Bu Chi Rou","Arthur Connan Doyle","Mo Xiang Tong Xiu","Cassandra Clare"};
+        int[] SeitenanzahlListe= {300-1000};
+        String[] TitelListe= {"The Husky and his white Cat shizun","Sherlock Holmes","Mo Dao Zu Shi","The Mortal Instruments"};
+        String[] JahrListe= {"1990","2020","2010","1850","1890"};
         for (int i =1; i<=100; i++) {
 
             int ci = r.nextInt(GenreListe.length -1 - 0) + 0;
@@ -19,28 +19,28 @@ public class Testit {
             int fi = r.nextInt(SeitenanzahlListe.length -1 - 0) + 0;
             int oi = r.nextInt(TitelListe.length -1 - 0) + 0;
             int ai = r.nextInt(JahrListe.length -1 - 0) + 0;
-            mKatzenListe.add(new Katze(charListe[ci],oberflaechListe[ni],alterListe[fi],farbeListe[oi],nameListe[ai]));
+            mBuchListe.add(new Buch(GenreListe[ci],AutorListe[ni],SeitenanzahlListe[fi],TitelListe[oi],JahrListe[ai]));
         }
-        katze.setAlter(1);
-        katze.setCharakter("Schmussekater");
-        katze.setFarbe("Pink mit blauen Streifen");
-        katze.setOberflaechenbeschaffenheit("flauschig");
-        katze.setName("Petti");
+        Buch.setSeitenanzahl(300);
+        Buch.setGenre("Danmei");
+        Buch.setTitel("The Husky and his white cat shizun");
+        Buch.setAutor("Rou Bao Bu Chi Rou");
+        Buch.setJahr("2022");
 
-        Katze katze2 = new Katze ("nette","nackte",5,"rosa","Sphinx");
-        mKatzenListe.add(katze2);
-        Haus haus = new Haus ( 350, "Buxdehude 4, 88732", 8, 5, maxkatzenalter >=5);
-        Haus haus2 = new Haus ( 400, "FU 3 6969", 10, 8);
-        haus.getInfo();
+        Buch buch2 = new Buch ("nette","nackte",5,"rosa","Sphinx");
+        mBuchListe.add(buch2);
+        Regal regal = new Regal ( 350, "Buxdehude 4, 88732", 8, 5, maxkatzenalter >=5);
+        Regal regal2 = new Regal ( 400, "FU 3 6969", 10, 8);
+        Regal.getInfo();
 
 
-        for(Katze k:mKatzenListe){
-            System.out.println("Meine Katze "+k.getName() + " ist " +k.getAlter() + " Jahre alt, sie ist eine " +k.getCharakter() +" " +k.getFarbe()
-                    +"farbige"  +  " "    +k.getOberflaechenbeschaffenheit() + " Katze.");
+        for(Buch k:mBuchListe){
+            System.out.println("Das Genre des Buches "+ this.Genre + " von dem Autor " + this.Autor + " hat den Namen "
+                    + this.Titel + " und ist insgesamt " + this.Seitenanzahl + " lang. Es erschien " + this.Jahr);
 
-            haus.addKatze(k);
+            Buch.addbuch(k);
         }
-        haus.getKatzenImHaus();
+        buch.getBuchRegal();
     }
 
 }
