e6b4127bd6a6378ada6d0dc26d2b65682b58df96
Anastasia Wohlmuth <anastasia.wohlmuth@student.htldornbirn.at>
2024-01-25 15:18:22 +0100

    Katze

--- src/Library/Testit.java
+++ src/Library/Testit.java
@@ -2,20 +2,31 @@ package Library;
 
 import java.util.ArrayList;
 import java.util.Random;
+
+import static Library.Buch.*;
+
 public class Testit {
     private static Buch buch;
     private static int ci;
+    private static int ni;
+    private static int fi;
+    private static int oi;
+    private static int ai;
 
     public static void main(String[]args) {
         Random r = new Random();
         ArrayList<Buch> mBuchListe = new ArrayList();
         String[] GenreListe = new String[0];
+        String[] AutorListe = new String[0];
+        int[] SeitenanzahlListe = new int[0];
+        String[] TitelListe = new String[0];
+        String[] JahrListe = new String[0];
         buch = new Buch(GenreListe[ci], AutorListe[ni], SeitenanzahlListe[fi], TitelListe[oi], JahrListe[ai]);
         GenreListe = new String[]{"Danmei", "Fantasy", "Horror", "Detektive"};
-        String[] AutorListe = {"Rou Bao Bu Chi Rou", "Arthur Connan Doyle", "Mo Xiang Tong Xiu", "Cassandra Clare"};
-        int[] SeitenanzahlListe = {300 - 1000};
-        String[] TitelListe = {"The Husky and his white Cat shizun", "Sherlock Holmes", "Mo Dao Zu Shi", "The Mortal Instruments"};
-        String[] JahrListe = {"1990", "2020", "2010", "1850", "1890"};
+        AutorListe = new String[]{"Rou Bao Bu Chi Rou", "Arthur Connan Doyle", "Mo Xiang Tong Xiu", "Cassandra Clare"};
+        SeitenanzahlListe = new int[]{300 - 1000};
+        TitelListe = new String[]{"The Husky and his white Cat shizun", "Sherlock Holmes", "Mo Dao Zu Shi", "The Mortal Instruments"};
+        JahrListe = new String[]{"1990", "2020", "2010", "1850", "1890"};
         for (int i = 1; i <= 100; i++) {
 
             int ci = r.nextInt(GenreListe.length - 1 - 0) + 0;
@@ -25,14 +36,11 @@ public class Testit {
             int ai = r.nextInt(JahrListe.length - 1 - 0) + 0;
             mBuchListe.add(new Buch(GenreListe[ci], AutorListe[ni], SeitenanzahlListe[fi], TitelListe[oi], JahrListe[ai]));
         }
-        Buch.setSeitenanzahl(300);
-        Buch.setGenre("Danmei");
-        Buch.setTitel("The Husky and his white cat shizun");
-        Buch.setAutor("Rou Bao Bu Chi Rou");
-        Buch.setJahr("2022");
+
 
         Buch buch2 = new Buch("nette", "nackte", 5, "rosa", "Sphinx");
         mBuchListe.add(buch2);
+        int maxkatzenalter;
         Regal regal = new Regal(350, "Buxdehude 4, 88732", 8, 5, maxkatzenalter >= 5);
         Regal regal2 = new Regal(400, "FU 3 6969", 10, 8);
         Regal.getInfo();
@@ -42,7 +50,7 @@ public class Testit {
             System.out.println("Das Genre des Buches " + this.Genre + " von dem Autor " + this.Autor + " hat den Namen "
                     + this.Titel + " und ist insgesamt " + this.Seitenanzahl + " lang. Es erschien " + this.Jahr);
 
-            Buch.addbuch(k);
+            addbuch(k);
         }
         buch.getMaxBuchRegal();
     }
